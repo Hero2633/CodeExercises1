cba36e0d62a004345ddc5032f9bd26da99fc8cfc
Hero2633 <2633105413@qq.com>
2022-03-28 12:14:50 +0800

    给定n个整数和一个整数x，小乐乐想从n个整数中判断x出现几次，请编程帮他实现。

---
+++ test.c
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int n =0 ;
+//	scanf("%d",&n);
+//	int arr[100] = { 0 };
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//
+//	}
+//	int x = 0;
+//	int count = 0;
+//	scanf("%d",&x);
+//	for (int j = 0; j < n; j++)
+//	{
+//		if (arr[j] == x)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d",count);
+//	return 0;
+//}
