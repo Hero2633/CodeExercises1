a8e224470a8fac78540589ed57e4e0b7cc302367
Hero2633 <2633105413@qq.com>
2022-04-06 21:33:18 +0800

    平时练习

---
+++ test5.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int tmp = 0;
+//	int arr[] = { 0,1,2,3,4,5,6,7,8,9 };
+//	int k = 3;
+//	for (int j = 1; j <= k; j++)
+//	{
+//		tmp = arr[9];
+//		int i = 0;
+//		for ( i = 10 - 1; i > 0; i--)
+//		{
+//			
+//			arr[i] = arr[i - 1];
+//
+//		}
+//		arr[i] = tmp;
+//	}
+//	for (int j = 0; j < 10; j++)
+//	{
+//		printf("%d ",arr[j]);
+//	}
+//	return 0;
+//}
+// 
+//int main()
+//{
+//	int a = 0;
+//	int sum = 0;
+//	int arr[10] = { 0 };
+//	for (int i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		sum = sum + arr[i];
+//	}int ret = 0;
+//
+//	for (int j = 0; j <=10; j++)
+//	{
+//		ret = ret + j;
+//	}
+//	printf("%d\n",ret-sum);
+//}
+// 
+//int main()
+//{
+//	int x = 0;
+//	int arr[7] = {0,1,2,3,4,5,7};
+//	for (int i = 0; i < 7; i++)
+//	{
+//		x ^= arr[i];
+//	}
+//	for (int i = 0; i <= 7; i += 1)
+//	{
+//		x ^= i;
+//	}
+//	printf("%d ",x);
+//	return 0;
+//}
